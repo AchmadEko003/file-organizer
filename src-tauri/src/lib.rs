894ac9892ccadead4548882d7dac7e48add47acb
madko <achmadeko003@gmail.com>
2025-09-25 22:13:12 +0700

    refactor: remove greet function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -3,11 +3,6 @@ mod helpers;
 mod pdf;
 
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
 fn get_root_path() -> String {
     let os = std::env::consts::OS;
 
@@ -20,7 +15,6 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            greet,
             get_root_path,
             folder_organize::get_list_of_files_in_folder,
             folder_organize::organize_folder,
